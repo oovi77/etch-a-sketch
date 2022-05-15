c9cf3f7fc3e23fe697f9a654ba5fac544786aac6
oovi77 <oovi77@gmail.com>
2022-05-15 11:34:51 -0400

    debugging querySelector issues

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("ya I'm an etch-a-sketch");
+
+const gridContainer = document.querySelector('.grid-container');
+
+console.log(gridContainer);
+
+const aPar = document.querySelector(".testP");
+console.log(aPar);
