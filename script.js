fe57eca8e58a795685927d16613167a2efe0b9a3
oovi77 <oovi77@gmail.com>
2022-05-22 16:38:48 -0400

    add set and reset grid buttons

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ console.log("ya I'm an etch-a-sketch");
 
 const gridContainer = document.querySelector(".grid-container");
 
-console.log(gridContainer);
+const numSquares = 0;
+
+const setButton = document.querySelector("#set-grid");
+const resetButton = document.querySelector("#reset-grid");
+
+
+
+/* -put grid creation in a function
+- set variable for making a X by X grid
+- event listeners for button
+- prompt user for input
+- hover event listener to change color of grids
+- reset grid button to redraw grid and clear all the
+squares /set them to original colors
+
+*/
+
 
 for (let j = 0; j <= 6; j++) {
     //create a row in this loop and then 
@@ -11,25 +27,32 @@ for (let j = 0; j <= 6; j++) {
 
     //still try to do the above even though this
     //somehow works
-    
+
+    //did the above, makes more sense now
+    //look into spaces between rows of divs
+
     const row = document.createElement('div');
     gridContainer.appendChild(row);
-    row.style.padding = "0px";
-    row.style.margin = "0px";
-    row.style.width  = "0px";
-    row.style.height  = "0px";
+    row.classList.add('row');
+    row.style.display = 'block';
+    /*
+    row.style.margin = '0px';
+    row.style.padding = '0px';
+    */
     
-
 
 
     for (let i = 0; i <= 6; i++) {
+
         const div = document.createElement('div');
         div.classList.add('square');
         div.style.display = 'inline-block';
-        gridContainer.appendChild(div);
 
-        gridContainer.style.margin = '0px';
-        gridContainer.style.padding = '0px';
+        
+
+        row.appendChild(div);
+
+        
 
         /*
         if (i === 6) {
