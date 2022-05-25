47584bd6d50b2de73483c75c7ce208a3baea50f5
oovi77 <oovi77@gmail.com>
2022-05-25 10:51:31 -0400

    Add Reset grid event listener to fix the issue where when the grid was created it would overflow and create a grid on top of the old grid

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ console.log("ya I'm an etch-a-sketch");
 
 const gridContainer = document.querySelector(".grid-container");
 
-const numSquares = 0;
+//const numSquares = 5;
 
 const setButton = document.querySelector("#set-grid");
 const resetButton = document.querySelector("#reset-grid");
 
 
+
+/* Problem, when grid gets created it doesn't
+make the proper amount of squares when using the
+set grid button
+- need to reset the grid after set grid is pressed
+
+
+*/
 
 /* -put grid creation in a function
 - set variable for making a X by X grid
@@ -20,48 +28,71 @@ squares /set them to original colors
 */
 
 
-for (let j = 0; j <= 6; j++) {
-    //create a row in this loop and then 
-    //append the divs created in the next loop
-    //to the row 
+setButton.addEventListener('click', createGrid);
+resetButton.addEventListener('click', resetGrid);
 
-    //still try to do the above even though this
-    //somehow works
+function resetGrid () {
 
-    //did the above, makes more sense now
-    //look into spaces between rows of divs
+    const allRows = document.querySelectorAll('.row');
+    console.log(allRows);
 
-    const row = document.createElement('div');
-    gridContainer.appendChild(row);
-    row.classList.add('row');
-    row.style.display = 'block';
-    /*
-    row.style.margin = '0px';
-    row.style.padding = '0px';
-    */
-    
-
-
-    for (let i = 0; i <= 6; i++) {
-
-        const div = document.createElement('div');
-        div.classList.add('square');
-        div.style.display = 'inline-block';
-
+    allRows.forEach(div => {
+        gridContainer.removeChild(div);
         
-
-        row.appendChild(div);
-
-        
-
-        /*
-        if (i === 6) {
-            div.style.display = "block";
-        }*/
-
-    }
+    });
 
 }
 
+function createGrid () {
+
+    resetGrid();
+
+    let numSquares = prompt("Please enter the number of squares per side for the grid", 4);
+
+    for (let j = 0; j < numSquares; j++) {
+        //create a row in this loop and then 
+        //append the divs created in the next loop
+        //to the row 
+
+        //still try to do the above even though this
+        //somehow works
+
+        //did the above, makes more sense now
+        //look into spaces between rows of divs
+
+        const row = document.createElement('div');
+        gridContainer.appendChild(row);
+        row.classList.add('row');
+        row.style.display = 'block';
+        /*
+        row.style.margin = '0px';
+        row.style.padding = '0px';
+        */
+        
+
+
+        for (let i = 0; i < numSquares; i++) {
+
+            const div = document.createElement('div');
+            div.classList.add('square');
+            div.style.display = 'inline-block';
+
+            
+
+            row.appendChild(div);
+
+            
+
+            /*
+            if (i === 6) {
+                div.style.display = "block";
+            }*/
+
+        }
+
+    }
+
+    
+}
 
 
